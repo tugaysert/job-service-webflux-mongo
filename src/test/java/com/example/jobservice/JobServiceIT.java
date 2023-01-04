134da322d1923256b8fc682e6acffef37ee9a51e
tugaysert <101674140+tugaysert@users.noreply.github.com>
2023-01-04 20:58:37 +0300

    all jobs integration test and remove boiler plate logs via logback-text.xml

---
+++ src/test/java/com/example/jobservice/JobServiceIT.java
@@ -0,0 +1,30 @@
+package com.example.jobservice;
+
+import com.example.jobservice.generic.BaseTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class JobServiceIT extends BaseTest {
+
+
+	@Autowired
+	private WebTestClient client;
+
+	@Test
+	public void allJobsTest() {
+		this.client.get()
+				.uri("/job/all")
+				.exchange()
+				.expectStatus()
+				.is2xxSuccessful()
+				.expectBody()
+				.consumeWith(e -> System.out.println(new String(e.getResponseBody())))
+				.jsonPath("$").isNotEmpty();
+	}
+
+}
