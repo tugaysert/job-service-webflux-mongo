d182b7bbfdde00d4794ca96ce134df29c103f08a
tugaysert <101674140+tugaysert@users.noreply.github.com>
2023-01-04 21:15:12 +0300

    add integration tests: searchBySkillTest and postJobTest

--- src/test/java/com/example/jobservice/JobServiceIT.java
+++ src/test/java/com/example/jobservice/JobServiceIT.java
@@ -1,11 +1,14 @@
 package com.example.jobservice;
 
+import com.example.jobservice.dto.JobDto;
 import com.example.jobservice.generic.BaseTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.Set;
 
 @SpringBootTest
 @AutoConfigureWebTestClient
@@ -25,6 +28,34 @@ class JobServiceIT extends BaseTest {
 				.expectBody()
 				.consumeWith(e -> System.out.println(new String(e.getResponseBody())))
 				.jsonPath("$").isNotEmpty();
+	}
+
+	@Test
+	public void searchBySkillTest() {
+		this.client.get()
+				.uri("/job/search?skills=java")
+				.exchange()
+				.expectStatus()
+				.is2xxSuccessful()
+				.expectBody()
+				.consumeWith(e -> System.out.println(new String(e.getResponseBody())))
+				.jsonPath("$.size()").isEqualTo(3);
+	}
+
+	@Test
+	public void postJobTest() {
+
+		JobDto dto = JobDto.create(null, "k8s engr", "google", Set.of("k8s"), 200000, true);
+		this.client.post()
+				.uri("/job")
+				.bodyValue(dto)
+				.exchange()
+				.expectStatus()
+				.is2xxSuccessful()
+				.expectBody()
+				.consumeWith(e -> System.out.println(new String(e.getResponseBody())))
+				.jsonPath("$.id").isNotEmpty()
+				.jsonPath("$.description").isEqualTo("k8s engr");
 	}
 
 }
